f1215e2d7dab0886ff2606c4404a0e48a16d810b
Gustavo Maia <ms.gustavo@outlook.com>
2025-03-09 00:11:14 -0300

    category

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+	    return repository.findById(id)
+	        .orElseThrow(() -> new EntityNotFoundException ("Category not found."));
+	}
+
+}
